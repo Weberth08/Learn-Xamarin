b1b9b3f92ed7c6658326ecded80cfd32349c4692
Weberth Fernandes <weberth.soares@aply.com.br>
2017-01-20 00:09:39 -0200

    Entity Framework configurations

---
+++ HouseChallenges/HouseChallenges.Data/Configs/ActivityExecutionConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using HouseChallenges.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace HouseChallenges.Data.Configs
+{
+    class ActivityExecutionConfiguration : EntityTypeConfiguration<ActivityExecution>
+    {
+        public ActivityExecutionConfiguration()
+        {
+            HasKey(act => act.Id);
+            ConfigureFields();
+            ConfigureRelationShip();
+        }
+
+        protected void ConfigureFields()
+        {
+
+            Property(p => p.ExecutionStatus)
+                .IsRequired();
+
+            Property(p => p.StartDateTime)
+               .IsOptional();
+
+            Property(p => p.EndDateTime)
+              .IsOptional();
+
+            Property(p => p.CancelDateTime)
+              .IsOptional();
+
+        }
+
+
+        public void ConfigureRelationShip()
+        {
+            // 1 A : N Person
+            HasRequired<Person>(p => p.Executor)
+                .WithMany()
+                .HasForeignKey(p => p.PersonId);
+
+            // 1 A : N Activity
+            HasRequired<Activity>(p => p.Activity)
+                .WithMany()
+                .HasForeignKey(p => p.ActivityId);
+        }
+    }
+
+}
