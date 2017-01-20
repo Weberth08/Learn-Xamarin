3939022bba7c4fafdf0f67cdcfb8eacbc330cb0f
Weberth Fernandes <weberth.soares@aply.com.br>
2017-01-20 00:09:56 -0200

    Context and UnitOfWork/EFUnitOfWork.cs

---
+++ HouseChallenges/HouseChallenges.Data/Context/EfHouseChallengesContext.cs
@@ -0,0 +1,53 @@
+﻿using HouseChallenges.Data.Configs;
+using HouseChallenges.Domain.Entities;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace HouseChallenges.Data.Context
+{
+    public class EfHouseChallengesContext : DbContext
+    {
+        public EfHouseChallengesContext() : base("HouseChallenges")
+        {
+
+        }
+
+        public DbSet<Activity> Activities;
+        public DbSet<ActivityExecution> ActivityExecutions;
+        public DbSet<HomeTask> HomeTasks;
+        public DbSet<House> Houses;
+        public DbSet<Person> Persons;
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+
+            modelBuilder.Configurations.Add(new ActivityExecutionConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        /*
+          public override int SaveChanges()
+          {
+              foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("CreationDate") != null))
+              {
+                  if (entry.State == EntityState.Added)
+                  {
+                      entry.Property("CreationDate").CurrentValue = DateTime.Now;
+                  }
+
+                  if (entry.State == EntityState.Modified)
+                  {
+                      entry.Property("CreationDate").IsModified = false;
+                  }
+
+              }
+              return base.SaveChanges();
+          }*/
+    }
+}
