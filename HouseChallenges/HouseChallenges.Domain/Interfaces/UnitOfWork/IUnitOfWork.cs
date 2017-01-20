13e6ad79eede320140f8fd4866cbbd666d85796c
Weberth Fernandes <weberth.soares@aply.com.br>
2017-01-20 00:05:24 -0200

    added IUnitOfWork.cs

---
+++ HouseChallenges/HouseChallenges.Domain/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using HouseChallenges.Domain.Interfaces.Repositories;
+using System;
+
+namespace HouseChallenges.Domain.Interfaces.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IActivityExecutionRepository ActivityExecutionRepository { get; }
+        IActivityRepository ActivityRepository { get; }
+        IPersonRepository PersonRepository { get; }
+        int Commit();
+
+    }
+}
